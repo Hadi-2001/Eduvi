82db5f4c82c2eb32bddfd8fb7691b7dbd2f98149
Hadi Sayadi <hadi1379.sa@gmail.com>
2023-07-21 08:51:52 +0330

    Initial configuration of routes and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 import './App.css'
 
 function App() {
-
+  const router = useRoutes(routes);
   return (
     <>
-      
+      {router}
     </>
   )
 }
